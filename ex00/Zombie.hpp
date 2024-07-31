1b490f0c37149a3be1d1b01d913d05f8588ba52e
eleekku <144835604+eleekku@users.noreply.github.com>
2024-07-31 15:19:27 +0300

    class def

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie{
+
+private:
+    std::string name;
+
+public:
+    void    announce(void);
+};
+
+#endif
