b533e8629e5d0dc1309a1ff26135dfc0896bb377
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2024-08-02 17:32:56 +0300

    ex04 done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,24 +1,60 @@
-#include "main.hpp"
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+
+int error_return(std::string message){
+    std::cout << message << std::endl;
+    return (1);
+}
+
+void    replace(std::string buffer, std::string s1, std::string s2, std::ofstream &outfile){
+        size_t  position;
+
+        position = buffer.find(s1);
+        if (position == std::string::npos){
+            outfile << buffer;
+            return;
+        }
+        else{
+            outfile << buffer.substr(0, position);
+            outfile << s2;
+            position = position + s1.length();
+            outfile << buffer.substr(position, buffer.length() - position);
+            outfile << "\n";
+        }
+}
 
 int main(int argc, char **argv){
         
     std::ifstream   fileIn;
-    std::ifstream   fileOut;
+    std::ofstream   fileOut;
     std::string     file;
     std::string     s1;
     std::string     s2;
+    std::string     buffer;
 
-    if (argc != 4){
-        std::cerr << "Please give three parameters in the following order: a"
-        << " filename and two strings, s1 and s2." << std::endl;
-    }
+    if (argc != 4)
+        return (error_return("Please give only three arguments file, s1, s2"));
     file = argv[1];
     fileIn.open(file.c_str());
-    if (!fileIn.good()){
-        std::cerr << "Eroror with input file" << std::endl;
-        return (1);
-    }
+    if (!fileIn.good())
+       return (error_return("Error with input fille"));
     s1 = argv[2];
     s2 = argv[3];
+    file = file + ".replace";
+    if (std::filesystem::exists(file)){
+        fileIn.close();
+        return (error_return("Error outputfile already exists")); 
+    }
+    fileOut.open(file.c_str());
+    if (!fileOut.good()){
+        fileIn.close();
+        return (error_return("Error with outfile"));
+    }
+    while (getline(fileIn, buffer))
+        replace(buffer, s1, s2, fileOut);
+    fileIn.close();
+    fileOut.close();
 
+    return (0);
 }
