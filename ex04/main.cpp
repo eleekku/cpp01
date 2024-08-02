7220c04609ff8998a1695d921e224405547ff60c
eleekku <144835604+eleekku@users.noreply.github.com>
2024-08-02 13:28:45 +0300

    working on ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -5,6 +5,8 @@ int main(int argc, char **argv){
     std::ifstream   fileIn;
     std::ifstream   fileOut;
     std::string     file;
+    std::string     s1;
+    std::string     s2;
 
     if (argc != 4){
         std::cerr << "Please give three parameters in the following order: a"
@@ -16,5 +18,7 @@ int main(int argc, char **argv){
         std::cerr << "Eroror with input file" << std::endl;
         return (1);
     }
+    s1 = argv[2];
+    s2 = argv[3];
 
 }
