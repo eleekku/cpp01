c78fd3e7ce9533cdf4ab7aeb935ab7d511e5b66a
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2024-08-06 00:35:10 +0300

    ex04 rdy

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,6 +1,7 @@
 #include <filesystem>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int error_return(std::string message){
     std::cout << message << std::endl;
@@ -10,51 +11,42 @@ int error_return(std::string message){
 void    replace(std::string buffer, std::string s1, std::string s2, std::ofstream &outfile){
         size_t  position;
 
+        while (1){
         position = buffer.find(s1);
         if (position == std::string::npos){
             outfile << buffer;
-            return;
+            break ;
         }
         else{
-            outfile << buffer.substr(0, position);
-            outfile << s2;
-            position = position + s1.length();
-            outfile << buffer.substr(position, buffer.length() - position);
-            outfile << "\n";
+            std::string cpy = buffer; 
+            cpy.insert(position, s2);
+            cpy = cpy.substr(0, position + s2.length());
+            outfile << cpy;
+            buffer = buffer.erase(0, position + s1.length());
+        }
         }
 }
 
 int main(int argc, char **argv){
-        
-    std::ifstream   fileIn;
-    std::ofstream   fileOut;
-    std::string     file;
     std::string     s1;
     std::string     s2;
-    std::string     buffer;
+    std::stringstream     buffer;
 
     if (argc != 4)
         return (error_return("Please give only three arguments file, s1, s2"));
-    file = argv[1];
-    fileIn.open(file.c_str());
-    if (!fileIn.good())
+    std::ifstream fileIn(argv[1]);
+    if (!fileIn.is_open())
        return (error_return("Error with input fille"));
     s1 = argv[2];
     s2 = argv[3];
-    file = file + ".replace";
-    if (std::filesystem::exists(file)){
-        fileIn.close();
-        return (error_return("Error outputfile already exists")); 
-    }
-    fileOut.open(file.c_str());
-    if (!fileOut.good()){
-        fileIn.close();
+    if (std::filesystem::exists("Outfile"))
+        return (error_return("Error outputfile already exists"));
+    std::ofstream fileOut("Outfile");
+    if (!fileOut.is_open())
         return (error_return("Error with outfile"));
-    }
-    while (getline(fileIn, buffer))
-        replace(buffer, s1, s2, fileOut);
-    fileIn.close();
-    fileOut.close();
+    buffer << fileIn.rdbuf();
+    std::string content = buffer.str();
+        replace(content, s1, s2, fileOut);
 
     return (0);
 }
